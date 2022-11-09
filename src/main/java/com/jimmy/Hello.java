6c3de6e66576e19e2733f3cc4b1f54599f72bb72
jimmy <good7700506@gmail.com>
2022-11-09 14:25:44 +0800

    Initial commit

---
+++ src/main/java/com/jimmy/Hello.java
@@ -0,0 +1,7 @@
+package com.jimmy;
+
+public class Hello {
+    public void Hello(){
+        System.out.println("Hello AA");
+    }
+}
---
+++ src/main/java/com/jimmy/Test.java
@@ -0,0 +1,8 @@
+package com.jimmy;
+
+public class Test {
+    public static void main(String args[]){
+        Hello h = new Hello();
+        h.Hello();
+    }
+}
