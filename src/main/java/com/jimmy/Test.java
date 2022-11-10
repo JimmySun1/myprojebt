f1a003213f7054e6459188f26c7f6b555e0cf956
jimmy <good7700506@gmail.com>
2022-11-10 10:00:54 +0800

    Java variable data type

--- src/main/java/com/jimmy/Test.java
+++ src/main/java/com/jimmy/Test.java
@@ -4,5 +4,12 @@ public class Test {
     public static void main(String args[]){
         Hello h = new Hello();
         h.Hello();
+        char a = 'A';
+        int age = 22;
+        byte b = 8;
+        float weight = 66.5f;
+        double hight = 1.7;
+        boolean enroll = true;
+        String name = "Jimmy";
     }
 }
